67d428891ca556ca00b2a2237e26140410603d26
cinothejk <marco.diblasi@external.stellantis.com>
2024-07-26 14:55:27 +0200

    Security Config

---
+++ SpringBootSecurity/src/main/java/com/cino/SpringBootSecurity/config/SecurityConfiguration.java
@@ -0,0 +1,29 @@
+package com.cino.SpringBootSecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.Customizer;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+	
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+			.authorizeHttpRequests(authConfig -> {
+				authConfig.requestMatchers("/").permitAll();
+				authConfig.requestMatchers("/user/**").authenticated();
+				authConfig.requestMatchers("/admin/**").denyAll();
+				
+			})
+			.formLogin(Customizer.withDefaults()) // Login with browser and Form
+			.httpBasic(Customizer.withDefaults()); // Login with Insomnia and Basic Auth
+		return http.build();
+	}
+	
+
+}
--- SpringBootSecurity/src/main/java/com/cino/SpringBootSecurity/controller/HomeController.java
+++ SpringBootSecurity/src/main/java/com/cino/SpringBootSecurity/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.cino.SpringBootSecurity.controller;
 
+
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +10,16 @@ public class HomeController {
 	
 	@GetMapping("/")
 	public String home() {
-		return "Welkom home!";
+		return "<h1>Welcome home!</h1>";
 	}
-
+	
+	@GetMapping("/user")
+	public String user(Authentication authentication) {
+		return "<h1>Welcome " + authentication.getName() + "!</h1>";
+	}
+	
+	@GetMapping("/admin")
+	public String admin(Authentication authentication) {
+		return "<h1>Welcome \" + authentication.getName() + \"!</h1>";
+	}
 }
